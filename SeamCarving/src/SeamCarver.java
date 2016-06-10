0fd77218937d727e6e9d04164a4637230ef41eb4
denis <redradist@gmail.com>
2016-06-10 07:40:28 +0300

    Added template for SeamCAving

---
+++ SeamCarving/src/SeamCarver.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author REDRADIST
+ */
+public class SeamCarver {
+   public SeamCarver(Picture picture)
+   {
+       
+   }
+   public Picture picture()
+   {
+       
+   }
+   public int width()
+   {
+       
+   }
+   public int height()
+   {
+       
+   }
+   public double energy(int x, int y)
+   {
+       
+   }
+   public int[] findHorizontalSeam()
+   {
+       
+   }
+   public int[] findVerticalSeam()
+   {
+       
+   }
+   public void removeHorizontalSeam(int[] seam)
+   {
+       
+   }
+   public void removeVerticalSeam(int[] seam)
+   {
+       
+   }
+}
