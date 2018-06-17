05e809794d766670f40dce058ca0b7e28d7ce053
redradist <redradist@gmail.com>
2018-06-17 11:50:46 +0300

    First succesfull implementation of MoveToFront algorithm

---
+++ Burrows–Wheeler/src/MoveToFront.java
@@ -0,0 +1,78 @@
+import edu.princeton.cs.algs4.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MoveToFront {
+    /**
+     * apply move-to-front encoding, reading from standard input and writing to standard output
+     */
+    public static void encode() {
+        List<Character> sequence = new LinkedList<>();
+        for (int i = 0; i < 256; ++i) {
+            sequence.add((char) i);
+        }
+        while (!StdIn.isEmpty()) {
+            char readCh = StdIn.readChar();
+            int pos = 0;
+            for (Character seqCh : sequence) {
+                if (readCh == seqCh) {
+                    StdOut.print(pos);
+                    StdOut.print(' ');
+                    break;
+                }
+                ++pos;
+            }
+            char foundCh = sequence.remove(pos);
+            sequence.add(0, foundCh);
+        }
+    }
+
+    /**
+     * apply move-to-front decoding, reading from standard input and writing to standard output
+     */
+    public static void decode() {
+        List<Character> sequence = new LinkedList<>();
+        for (int i = 0; i < 256; ++i) {
+            sequence.add((char) i);
+        }
+
+        while (!StdIn.isEmpty()) {
+            StringBuilder number = new StringBuilder();
+            char ch;
+            while(!StdIn.isEmpty() && (ch = StdIn.readChar()) != ' ') {
+                number.append(ch);
+            }
+            int posCh = Integer.parseInt(number.toString());
+            int pos = 0;
+            for (Character seqCh : sequence) {
+                if (pos == posCh) {
+                    StdOut.print(seqCh);
+                    break;
+                }
+                ++pos;
+            }
+            char foundCh = sequence.remove(pos);
+            sequence.add(0, foundCh);
+        }
+    }
+
+    /**
+     * if args[0] is '-', apply move-to-front encoding
+     * if args[0] is '+', apply move-to-front decoding
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        if (args.length >= 1) {
+            if (args[0].compareTo("-") == 0) {
+                MoveToFront.encode();
+            } else if (args[0].compareTo("+") == 0) {
+                MoveToFront.decode();
+            }
+        }
+    }
+}
